d1f4c1d1550eca70970c4bed65b86454539fedb0
FareedSyed31 <fareed.syed31@gmail.com>
2021-11-17 16:36:45 -0500

    header .ino file created

---
+++ MEC830_Robot_Sec01_Group03/MEC830_Robot_Sec01_Group03.ino
@@ -0,0 +1,15 @@
+// MEC830 - Robot Car
+
+// Contributers:
+// Fareed Syed - 500819438
+// Add your name - student#
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
