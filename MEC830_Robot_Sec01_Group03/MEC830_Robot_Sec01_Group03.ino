147843d9280bacca73355c856a96036eaf3d328f
FareedSyed31 <fareed.syed31@gmail.com>
2021-11-28 19:25:02 -0500

    added code for Task_4

--- MEC830_Robot_Sec01_Group03/MEC830_Robot_Sec01_Group03.ino
+++ MEC830_Robot_Sec01_Group03/MEC830_Robot_Sec01_Group03.ino
@@ -28,26 +28,15 @@ void setup() {
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
 
-  stepper.setSpeed(50);
+  stepper.setSpeed(100);
 }
 
 void loop() {
-
-  while (stop == false) {
-    directionControl();
-    delay(2000);
-    stepper.step(2000);
-    delay(2000);
-    i++;
-    if (i == 4) {
-      stop = true;
-    }
-
-  }
+  Task_4();
 }
 
 // This function lets you control spinning direction of motors
-void directionControl() {
+void run_DC_motor() {
   // Set motors to maximum speed
   // For PWM maximum possible values are 0 to 255
   analogWrite(enB, 255);
@@ -64,25 +53,17 @@ void directionControl() {
 
 }
 
-// This function lets you control speed of the motors
-void speedControl() {
-  // Turn on motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
+void Task_4() {
+  delay(2000);
+  while (stop == false) {
+    run_DC_motor();
+    //    delay(1000);
+    stepper.step(2500);
+    delay(2000);
+    i++;
+    if (i == 4) {
+      stop = true;
+    }
 
-  // Accelerate from zero to maximum speed
-  for (int i = 0; i < 256; i++) {
-    analogWrite(enB, i);
-    delay(20);
   }
-
-  // Decelerate from maximum speed to zero
-  for (int i = 255; i >= 0; --i) {
-    analogWrite(enB, i);
-    delay(20);
-  }
-
-  // Now turn off motors
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
 }
